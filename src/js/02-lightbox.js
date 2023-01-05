f3bbe5f88308ac9aa18f06d3515c129cd04db7e9
IrynaRozghon <irina.rozgon49@gmail.com>
2023-01-05 23:06:46 +0200

    2 task

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryElement = document.querySelector('.gallery');
+
+function createImgInMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+        .join('');
+   
+}
+galleryElement.insertAdjacentHTML('beforeend', createImgInMarkup(galleryItems));
+
+const gallery = new simpleLightbox('.gallery.a', {
+    caption: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
